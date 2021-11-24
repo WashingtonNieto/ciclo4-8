fd3f0af0e505f7ae0f60554b4d692247988993c7
Washington Nieto <washington_nieto@hotmail.com>
2021-11-24 18:35:47 -0500

    First commit

---
+++ src/main/java/com/ciclo48/ciclo48/Ciclo48Application.java
@@ -0,0 +1,13 @@
+package com.ciclo48.ciclo48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo48Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciclo48/ciclo48/Ciclo48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo48.ciclo48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
